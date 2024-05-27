d78b4e3a4ff54255fddb07a1a161a3a65d0aec2d
Sepehr Mansouri <Sepehrman@icloud.com>
2024-05-27 15:25:08 -0700

    AI course

---
+++ assignment_1/src/MyClass.java
@@ -0,0 +1,27 @@
+public class MyClass {
+    public static void main(String[] args) {
+        char current_location = args[0].charAt(0);
+        //state: true -> clean, false -> not clean
+        boolean A_status = Boolean.parseBoolean(args[1]);
+        boolean B_status = Boolean.parseBoolean(args[2]);
+        boolean C_status = Boolean.parseBoolean(args[3]);
+        boolean D_status = Boolean.parseBoolean(args[4]);
+
+        System.out.println("Current Location = " + current_location + "\n" +
+                "Square A status = " + A_status + "\n"+
+                "Square B status = " + B_status + "\n"+
+                "Square C status = " + C_status + "\n"+
+                "Square D status = " + D_status + "\n");
+        // ----------------
+        // Assignment 1: Model-based Agent Function
+        // ----------------
+
+        
+
+
+
+
+
+        System.out.println("\nAction - Next Location = ");
+    }
+}
