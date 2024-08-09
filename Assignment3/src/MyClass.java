376459cc726d93b85ae288c312d3b94ba1fb6831
Sepehrman <sepehrman@icloud.com>
2024-08-08 18:04:45 -0700

    added heuristic function. WIP

--- Assignment3/src/MyClass.java
+++ Assignment3/src/MyClass.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class MyClass {
 
     int[][] adjacencyMatrix = {
@@ -18,42 +20,26 @@ public class MyClass {
 
     // Array for initial state, S0
     char[] currentState = {'b', 'o', 'o', 'o', 'r', 'r', 'j', 'j', 'j', 'j', 'j', 'j', 'j'};
-    final char[] INITIAL_STATE = {'b', 'o', 'o', 'o', 'r', 'r', 'j', 'j', 'j', 'j', 'j', 'j', 'j'};
-    char[] colours; // blue, orange, red, jungle
+    char[] colours = {'b', 'o', 'r', 'j'};
     int kValue;
 
 
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Please provide the k-Value as a Command Line Argument.");
-            return;
-        }
+    public char[] successorFunction() {
+        Random rand = new Random();
+        char[] newState = currentState.clone();
 
-        int k = Integer.parseInt(args[0]);
-        MyClass myClass = new MyClass(k);
-        myClass.runHillClimbing();
-    }
+        // Randomly select a region to change its color
+        int regionToChange = rand.nextInt(currentState.length);
+        char currentColor = newState[regionToChange];
 
-    /**
-     * Initializes the Initial State of our map
-     * @param k
-     */
-    public MyClass(int k) {
-        this.kValue = k;
-        // Only allow the first k colours from the available set of colours
-        char[] allColours = INITIAL_STATE;
-        this.colours = new char[k];
-        System.arraycopy(allColours, 0, colours, 0, k);
+        // Ensure that the new color is different from the current color
+        char newColor;
+        do {
+            newColor = colours[rand.nextInt(kValue)];
+        } while (newColor == currentColor);
 
-        System.out.print("Initial state: ");
-        printStateArray(currentState);
-
-        System.out.println(costFunction());
-    }
-
-
-    public void successorFunction() {
-
+        newState[regionToChange] = newColor;
+        return newState;
     }
 
     public int costFunction() {
@@ -85,13 +71,48 @@ public class MyClass {
         return cost;
     }
 
-
-    public void heuristicFunction() {
-
+    /**
+     * Heuristic Function that finds the number of adjacent regions that share the same color
+     * @param currentState Array of Characters of the current state
+     * @return an Integer that represents the number of adjacent regions that share the same color.
+     */
+    public int heuristicFunction(char[] currentState) {
+        int counter = 0;
+        for (int i = 0; i < currentState.length; i++) {
+            for (int j = i + 1; j < currentState.length; j++) {
+                // Check if there are adjacent regions and share the same color
+                if (adjacencyMatrix[i][j] == 1 && currentState[i] == currentState[j]) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 
     public void runHillClimbing() {
 
+        int currentHeuristic = heuristicFunction(currentState);
+        int currentCost = costFunction();
+
+        while (currentHeuristic > 0) {
+            char[] nextState = successorFunction();
+            int nextHeuristic = heuristicFunction(nextState);
+            int nextCost = costFunction();
+
+            // Move to the successor state if it has a better heuristic or equal heuristic but lower cost
+            if (nextHeuristic < currentHeuristic || (nextHeuristic == currentHeuristic && nextCost < currentCost)) {
+                currentState = nextState;
+                currentHeuristic = nextHeuristic;
+                currentCost = nextCost;
+            } else {
+                break; // Local maximum reached
+            }
+        }
+
+        System.out.println("Final state after hill climbing:");
+        printStateArray(currentState);
+        System.out.println("Heuristic: " + currentHeuristic);
+        System.out.println("Total cost: " + currentCost);
 
     }
 
@@ -100,6 +121,36 @@ public class MyClass {
             System.out.print(c + " ");
         }
         System.out.println();
+    }
+
+    /**
+     * Check if a matrix is a goal state
+     * @return a Boolean representing if a goal state has reached
+     */
+    public boolean hasGoalState() {
+        return heuristicFunction(currentState) == 0;
+    }
+
+
+    /**
+     * Initializes the Initial State of our map
+     * @param k An Integer representing the K-Value
+     */
+    public MyClass(int k) {
+        this.kValue = k;
+        // Only allow the first k colours from the available set of colours
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Please provide the k-Value as a Command Line Argument.");
+            return;
+        }
+
+        int k = Integer.parseInt(args[0]);
+        MyClass myClass = new MyClass(k);
+        myClass.runHillClimbing();
+
     }
 
 }
