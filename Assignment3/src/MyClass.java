b419cde5c96a2b1ec3185e24e89f84f973992392
Sepehrman <sepehrman@icloud.com>
2024-08-09 00:03:17 -0700

    fix for finding goal state

--- Assignment3/src/MyClass.java
+++ Assignment3/src/MyClass.java
@@ -67,7 +67,6 @@ public class MyClass {
                 cost = 5;
                 break;
         }
-
         return cost;
     }
 
@@ -86,6 +85,7 @@ public class MyClass {
                 }
             }
         }
+        System.out.println(counter);
         return counter;
     }
 
